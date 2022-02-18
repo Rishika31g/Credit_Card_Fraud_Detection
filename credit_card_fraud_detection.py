6ed1e0970d22d0fafef690755bfa209f48bfd406
Rishika Gupta <84868157+Rishika31g@users.noreply.github.com>
2022-02-18 18:42:46 +0530

    Update credit_card_fraud_detection.py

--- credit_card_fraud_detection.py
+++ credit_card_fraud_detection.py
@@ -7,3 +7,169 @@ import seaborn as sns
 
 import warnings
 warnings.filterwarnings('ignore')
+
+pd.set_option('display.max_columns', 500)
+
+df = pd.read_csv('creditcard.csv')
+df.head()
+
+# df.shape
+# df.info()
+
+# Cheking percent of missing values in columns
+df_missing_columns = (round(((df.isnull().sum()/len(df.index))*100),2).to_frame('null')).sort_values('null', ascending=False)
+df_missing_columns
+
+classes = df['Class'].value_counts()
+
+normal_share = round((classes[0]/df['Class'].count()*100),2)
+
+fraud_share = round((classes[1]/df['Class'].count()*100),2)
+
+sns.countplot(x='Class', data=df)
+plt.title('Number of fraudulent vs non-fraudulent transcations')
+plt.show()
+
+fraud_percentage = {'Class':['Non-Fraudulent', 'Fraudulent'], 'Percentage':[normal_share, fraud_share]} 
+df_fraud_percentage = pd.DataFrame(fraud_percentage) 
+sns.barplot(x='Class',y='Percentage', data=df_fraud_percentage)
+plt.title('Percentage of fraudulent vs non-fraudulent transcations')
+plt.show()
+
+# Creating fraudulent dataframe
+data_fraud = df[df['Class'] == 1]
+# Creating non fraudulent dataframe
+data_non_fraud = df[df['Class'] == 0]
+
+plt.figure(figsize=(8,5))
+ax = sns.distplot(data_fraud['Time'],label='fraudulent',hist=False)
+ax = sns.distplot(data_non_fraud['Time'],label='non fraudulent',hist=False)
+ax.set(xlabel='Seconds elapsed between the transction and the first transction')
+plt.show()
+
+# Dropping the Time column
+df.drop('Time', axis=1, inplace=True)
+
+# Distribution plot
+plt.figure(figsize=(8,5))
+ax = sns.distplot(data_fraud['Amount'],label='fraudulent',hist=False)
+ax = sns.distplot(data_non_fraud['Time'],label='non fraudulent',hist=False)
+ax.set(xlabel='Transction Amount')
+plt.show()
+
+from sklearn.model_selection import train_test_split
+# Putting feature variables into X
+X = df.drop(['Class'], axis=1)
+
+# Putting target variable to y
+y = df['Class']
+
+# Splitting data into train and test set 80:20
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=100)
+
+from sklearn.preprocessing import StandardScaler
+
+# Instantiate the Scaler
+scaler = StandardScaler()
+
+# Fit the data into scaler and transform
+X_train['Amount'] = scaler.fit_transform(X_train[['Amount']])
+X_train.head()
+
+X_test['Amount'] = scaler.transform(X_test[['Amount']])
+X_test.head()
+
+cols = X_train.columns
+
+k=0
+plt.figure(figsize=(17,28))
+for col in cols :    
+    k=k+1
+    plt.subplot(6, 5,k)    
+    sns.distplot(X_train[col])
+    plt.title(col+' '+str(X_train[col].skew()))
+
+from sklearn.preprocessing import PowerTransformer
+
+# Instantiate the powertransformer
+pt = PowerTransformer(method='yeo-johnson', standardize=True, copy=False)
+
+# Fit and transform the PT on training data
+X_train[cols] = pt.fit_transform(X_train)
+X_test[cols] = pt.transform(X_test)
+
+k=0
+plt.figure(figsize=(17,28))
+
+for col in cols :    
+    k=k+1
+    plt.subplot(6, 5,k)    
+    sns.distplot(X_train[col])
+    plt.title(col+' '+str(X_train[col].skew()))
+    
+from sklearn.linear_model import LogisticRegression
+
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
+from sklearn.metrics import classification_report
+
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+
+folds = KFold(n_splits=5, shuffle=True, random_state=4)
+
+# Specify params
+params = {"C": [0.01, 0.1, 1, 10, 100, 1000]}
+
+# Specifing score as recall as we are more focused on acheiving the higher sensitivity than the accuracy
+model_cv = GridSearchCV(estimator = LogisticRegression(),
+                        param_grid = params, 
+                        scoring= 'roc_auc', 
+                        cv = folds, 
+                        verbose = 1,
+                        return_train_score=True) 
+
+# Fit the model
+model_cv.fit(X_train, y_train)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
